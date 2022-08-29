878d21c6740b2e2930b7a67418557e76aa05ac9c
arturs jersovs <freelance.jershov@gmail.com>
2022-08-29 15:32:52 +0300

    test

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,5 @@
 export async function load() {
+  console.log("hello");
   const response = await fetch("https://cms.sghomedecor.lv/api/products");
   const products = await response.json();
   return {
