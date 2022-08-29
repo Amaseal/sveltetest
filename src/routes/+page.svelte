7ce9fd60d369a0ce375c09a8583e65c159bf531b
arturs jersovs <freelance.jershov@gmail.com>
2022-08-29 05:49:19 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  let response;
+  const update = async () => {
+    const res = await fetch(
+      "https://cms.sghomedecor.lv/api/home?populate=background,quotes,about,about.image"
+    );
+    let responseData = await res.json();
+
+    response = responseData.data;
+  };
+</script>
+
+<button on:click={() => update()}>fetch</button>
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+{response}
